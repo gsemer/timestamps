b08dcbee4d7f2780770c31342577c4c9136844a9
gsemer <gsemertzakis@hotmail.com>
2023-01-28 22:34:49 +0200

    monthly and daily matching timestamps

--- persistence/dates.go
+++ persistence/dates.go
@@ -1,7 +1,6 @@
 package persistence
 
 import (
-	"fmt"
 	"time"
 )
 
@@ -13,42 +12,82 @@ func NewTimestampsRepository() *TimestampRepository {
 
 func (tr TimestampRepository) Hour(period, tmp1, tmp2, loc string) ([]time.Time, error) {
 	layout := "20060102T150405Z"
-
+	// set timezone to input parameter loc
 	location, err := time.LoadLocation(loc)
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
-
-	// Parse the timestamps
+	// parse the timestamps
 	t1, err := time.ParseInLocation(layout, tmp1, location)
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
 	t2, err := time.ParseInLocation(layout, tmp2, location)
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
-
-	// Initialize a slice to store the timestamps
+	// initialize a slice to store the timestamps
 	var timestamps []time.Time
-
-	// Iterate over the time period of one hour
+	// iterate over the time period of one hour
 	hour, _ := time.ParseDuration(period)
 	for t := t1; t.Before(t2); t = t.Add(hour) {
 		timestamps = append(timestamps, t)
 	}
-
 	return timestamps, nil
 }
 
-func (tr TimestampRepository) Day() error {
-	return nil
+func (tr TimestampRepository) Day(period, tmp1, tmp2, loc string) ([]time.Time, error) {
+	layout := "20060102T150405Z"
+	// set timezone to input parameter loc
+	location, err := time.LoadLocation(loc)
+	if err != nil {
+		return nil, err
+	}
+	// parse the timestamps
+	t1, err := time.ParseInLocation(layout, tmp1, location)
+	if err != nil {
+		return nil, err
+	}
+	t2, err := time.ParseInLocation(layout, tmp2, location)
+	if err != nil {
+		return nil, err
+	}
+	// initialize a slice to store the timestamps
+	var timestamps []time.Time
+	// Iterate over the time period of one day
+	for t := t1; t.Before(t2); {
+		timestamps = append(timestamps, t)
+		t = t.AddDate(0, 0, 1)
+	}
+	return timestamps, nil
 }
 
-func (tr TimestampRepository) Month() error {
-	return nil
+func (tr TimestampRepository) Month(period, tmp1, tmp2, loc string) ([]time.Time, error) {
+	layout := "20060102T150405Z"
+	// set timezone to input parameter loc
+	location, err := time.LoadLocation(loc)
+	if err != nil {
+		return nil, err
+	}
+	// parse the timestamps
+	t1, err := time.ParseInLocation(layout, tmp1, location)
+	if err != nil {
+		return nil, err
+	}
+	t2, err := time.ParseInLocation(layout, tmp2, location)
+	if err != nil {
+		return nil, err
+	}
+	// initialize a slice to store the timestamps
+	var timestamps []time.Time
+	// iterate over the time period of one month
+	month := time.Duration(30 * 24 * time.Hour)
+	for t := t1; t.Before(t2); t = t.Add(month) {
+		timestamps = append(timestamps, t)
+	}
+	return timestamps, nil
 }
 
-func (tr TimestampRepository) Year() error {
-	return nil
+func (tr TimestampRepository) Year(period, tmp1, tmp2, loc string) ([]time.Time, error) {
+	return nil, nil
 }
