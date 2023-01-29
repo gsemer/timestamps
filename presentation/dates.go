d923399721ffdb4cdf194eac8e36368c3c7649fd
gsemer <gsemertzakis@hotmail.com>
2023-01-29 13:10:41 +0200

    fix the response

--- presentation/dates.go
+++ presentation/dates.go
@@ -14,12 +14,17 @@ func NewTimestampsHandler(ts domain.TimestampsService) *TimestampHandler {
 	return &TimestampHandler{ts: ts}
 }
 
+type MatchingTimestampsResponse struct {
+	Data []string `json:"data"`
+}
+
 func (th TimestampHandler) GetMatchingTimestamps(writer http.ResponseWriter, request *http.Request) {
 	p, found1 := request.URL.Query()["period"]
 	t1, found2 := request.URL.Query()["t1"]
 	t2, found3 := request.URL.Query()["t2"]
 	tz, found4 := request.URL.Query()["tz"]
 	var period, tmp1, tmp2, loc string
+
 	if found1 && found2 && found3 && found4 {
 		period = p[0]
 		tmp1 = t1[0]
@@ -42,8 +47,9 @@ func (th TimestampHandler) GetMatchingTimestamps(writer http.ResponseWriter, req
 		writer.Write(resultErr)
 		return
 	}
+
 	writer.WriteHeader(http.StatusOK)
-	matchingTimestamps := domain.MatchingTimestamps{Data: timestamps}
+	matchingTimestamps := MatchingTimestampsResponse{Data: timestamps}
 	result, _ := json.Marshal(matchingTimestamps)
 	writer.Write(result)
 	return
