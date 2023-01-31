2006b10c50e185572f3c75ca21425dee48468ac6
gsemer <gsemertzakis@hotmail.com>
2023-01-31 21:44:35 +0200

    make the service extensible

--- app/services/dates.go
+++ app/services/dates.go
@@ -2,6 +2,8 @@ package services
 
 import (
 	"errors"
+	"strings"
+	"time"
 	"timestamps/domain"
 )
 
@@ -14,27 +16,46 @@ func NewTimestampsService(tr domain.TimestampsRepository) *TimestampService {
 }
 
 func (ts TimestampService) GetMatchingTimestamps(period, tmp1, tmp2, loc, layout string) ([]string, error) {
-	switch period {
-	case "1h":
-		timestamps, err := ts.tr.Hour(period, tmp1, tmp2, loc, layout)
+	// set timezone to input parameter loc
+	location, err := time.LoadLocation(loc)
+	if err != nil {
+		return nil, err
+	}
+	// parse the timestamps
+	t1, err := time.ParseInLocation(layout, tmp1, location)
+	if err != nil {
+		return nil, err
+	}
+	t2, err := time.ParseInLocation(layout, tmp2, location)
+	if err != nil {
+		return nil, err
+	}
+	// check if t1 is after t2
+	if t1.After(t2) {
+		return nil, errors.New("invalid query parameters: t1 must be less than t2")
+	}
+	// handle every case separately based on the period
+	switch {
+	case strings.Contains(period, "h"):
+		timestamps, err := ts.tr.Hour(period, t1, t2, location, layout)
 		if err != nil {
 			return nil, err
 		}
 		return timestamps, nil
-	case "1d":
-		timestamps, err := ts.tr.Day(period, tmp1, tmp2, loc, layout)
+	case strings.Contains(period, "d"):
+		timestamps, err := ts.tr.Day(period, t1, t2, location, layout)
 		if err != nil {
 			return nil, err
 		}
 		return timestamps, nil
-	case "1mo":
-		timestamps, err := ts.tr.Month(period, tmp1, tmp2, loc, layout)
+	case strings.Contains(period, "mo"):
+		timestamps, err := ts.tr.Month(period, t1, t2, location, layout)
 		if err != nil {
 			return nil, err
 		}
 		return timestamps, nil
-	case "1y":
-		timestamps, err := ts.tr.Year(period, tmp1, tmp2, loc, layout)
+	case strings.Contains(period, "y"):
+		timestamps, err := ts.tr.Year(period, t1, t2, location, layout)
 		if err != nil {
 			return nil, err
 		}
